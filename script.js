96700ec2cd65ba32bc108da9e7eb1d9e0fa2d5ce
Mark Ferianec <134004771+MarkFerianec@users.noreply.github.com>
2023-10-14 22:41:46 -0600

    Modify index, script and style
    Added elements for different rows of gameboard. Added basic styling
    for gameboard.

--- script.js
+++ script.js
@@ -3,26 +3,57 @@ const gameBoard = (function () {
     return { gameBoardArray };
 })();
 
-const gameBoardDisplay = document.querySelector('.gameboard');
-const gameBoardFirstColumn = document.querySelector('.firstcolumn');
-const gameBoardSecondColumn = document.querySelector('.secondcolumn');
-const gameBoardThirdColumn = document.querySelector('.thirdcolumn');
+// const gameBoardDisplay = document.querySelector('.gameboard');
+// const gameBoardFirstColumn = document.querySelector('.firstcolumn');
+// const gameBoardSecondColumn = document.querySelector('.secondcolumn');
+// const gameBoardThirdColumn = document.querySelector('.thirdcolumn');
 
-const displayController = (function () {
-    for (let i = 0; i < gameBoard.gameBoardArray.length; i++) {
-        const div = document.createElement('div');
-        div.style.padding = '50px';
-        div.textContent = gameBoard.gameBoardArray[i];
-        console.log(gameBoard.gameBoardArray[i]);
-        if (i < 3) {
-            gameBoardFirstColumn.appendChild(div)
-        }
-        else if (i < 6) {
-            gameBoardSecondColumn.appendChild(div);
-        }
-        else {
-            gameBoardThirdColumn.appendChild(div);
-        }
-    }
-})();
+// function createPlayer(name, marker) {
+//     // function markSpot (marker) {
+//     // gameBoardFirstColumn.addEventListener('click', toggleMarker);
+
+//     // function toggleMarker() {
+//     // gameBoardFirstColumn.textContent = marker;
+//     // }
+//     // }
+
+//     // return { name, marker, markSpot };
+//     return { name, marker };
+// }
+
+// maybe something that creates player objects using typed input from users?
+// read input from user, create object using inputs. Player 1 is first input, player 2 is second input.
+
+// player1 = createPlayer("Mark", "X");
+// player2 = createPlayer("Someone", "O");
+// console.log(player1);
+// console.log(player2);
+
+// const displayController = (function () {
+//     for (let i = 0; i < gameBoard.gameBoardArray.length; i++) {
+//         const div = document.createElement('div');
+//         div.style.padding = '50px';
+//         div.textContent = gameBoard.gameBoardArray[i];
+//         div.addEventListener('click', toggleMarker);
+
+//         console.log(gameBoard.gameBoardArray[i]);
+
+//         if (i < 3) {
+//         gameBoardFirstColumn.appendChild(div)
+//         }
+//         else if (i < 6) {
+//         gameBoardSecondColumn.appendChild(div);
+//         }
+//         else {
+//         gameBoardThirdColumn.appendChild(div);
+//         }
+//         function toggleMarker() {
+            
+//             div.textContent = player1.marker;
+//             // div.textContent = "X";
+//         }
+//     }
+// })();
+
+
 
